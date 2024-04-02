c6b657b3fadd6a3e21a863c1a0fd9ee427953ffb
shabzw <62140577+shabzw@users.noreply.github.com>
2024-04-02 11:28:39 +0530

    Update MyUserController.ts

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -22,9 +22,9 @@ const createCurrentUser = async(req: Request, res: Response) => {
     // 2. create the user if it doesn't exist
     // 3. return the user to the calling client
     try{
-        const {auth0Id} = req.body;
+        const {auth0Id, email} = req.body;
         console.log(auth0Id+"This is authID")
-        const existingUser = await User.findOne({auth0Id})
+        const existingUser = await User.findOne({email})
         console.log(existingUser)
         
         if(existingUser){
